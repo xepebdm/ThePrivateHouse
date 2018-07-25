d21be0d7eeef6539294823aee894b1600d4b00fd
Alexsandre Rodrigues <xepe_style@hotmail.com>
2018-07-24 21:47:20 -0300

    Update

--- src/main/java/com/maven/privatehouse/user/UserBean.java
+++ src/main/java/com/maven/privatehouse/user/UserBean.java
@@ -1,15 +1,5 @@
 package com.maven.privatehouse.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class UserBean {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
 
 }
